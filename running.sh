bfcd1e819a0e4856b757b247450fe11a6d0c78a6
竟康 <jinglekang@live.cn>
2026-03-04 17:14:32 +0800

    固定版本号并适应wsl环境

--- running.sh
+++ running.sh
@@ -1,5 +1,5 @@
 # 构建镜像
-docker build -t cloudreve:latest . --build-arg "HTTPS_PROXY=http://192.168.81.232:7890"
+# docker build -t cloudreve:3.8.3 . --build-arg "HTTPS_PROXY=http://192.168.81.100:7890"
 # 删除构件中的临时镜像
 docker image prune -f
 # 停止旧容器
@@ -7,4 +7,4 @@ docker stop cloudreve
 # 删除老容器
 docker rm cloudreve
 # 启动服务
-docker run -d --name cloudreve -p 5200:5200 -v /root/docker/cloudreve/cloudreve.db:/cloudreve/cloudreve.db -v /root/docker/cloudreve/conf.ini:/cloudreve/conf.ini -v /root/docker/cloudreve/avatar:/cloudreve/avatar -v /root/cloudreve/uploads:/cloudreve/uploads --restart unless-stopped cloudreve:latest
+docker run -d --name cloudreve -p 5200:5200 -v /mnt/host/d/docker/cloudreve/cloudreve.db:/cloudreve/cloudreve.db -v /mnt/host/d/docker/cloudreve/conf.ini:/cloudreve/conf.ini -v /mnt/host/d//docker/cloudreve/avatar:/cloudreve/avatar -v /mnt/host/d//docker/cloudreve/uploads:/cloudreve/uploads --restart unless-stopped cloudreve:3.8.3
